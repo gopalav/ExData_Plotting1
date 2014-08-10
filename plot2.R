ab7460f9652680f6c527342393b6b080f01b49e4
Venugopal Ambadipudi <gopalav.gmail.com>
2014-08-10 15:05:38 -0700

    Fixing typo

--- plot2.R
+++ plot2.R
@@ -5,7 +5,7 @@ names<-c("Date","Time","Global_active_power","Global_reactive_power","Voltage","
 # When we are reading intermediate rows, we will miss the header, so manually add the column names
 powerData<-read.table("UCIPower/household_power_consumption.txt",header=TRUE,sep=";",skip=66636,nrows=2880, col.names = names, na.strings = "?")
 
-# X axix is for the date time.  Create a new variable from date and time
+# X axis is for the date time.  Create a new variable from date and time
 dateTime <- strptime(paste(powerData$Date, powerData$Time), "%d/%m/%Y %H:%M:%S")
 
 # Create a plot for active power on y axis and date time on x axis
